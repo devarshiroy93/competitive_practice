47ef9b5f3fdf4780dc8546e731aa30d16591e2a1
devarshiroy93 <devarshiroy93@gmail.com>
2022-12-11 21:51:01 +0530

    extract max

--- linked_lists/binaryheap.js
+++ linked_lists/binaryheap.js
@@ -1,37 +1,74 @@
-class MaxBinaryHeap{
-    constructor(){
+class MaxBinaryHeap {
+    constructor() {
         this.values = [];
     }
 
-    insert(val){
+    insert(val) {
         this.values.push(val);
         let currentIndex = this.values.length - 1;
-        const bubble = (latestIndex)=>{
+        const bubble = (latestIndex) => {
             const parentIndex = this.getParentIndex(latestIndex);
-            if ( this.values[parentIndex] < this.values[latestIndex]){
-                this.values = this.swap(latestIndex , parentIndex , this.values);
+            if (this.values[parentIndex] < this.values[latestIndex]) {
+                this.values = this.swap(latestIndex, parentIndex, this.values);
                 bubble(parentIndex)
-            }else{
+            } else {
                 return this.values;
             }
         }
-        if(this.values.length >1 )bubble(currentIndex)
-        
+        if (this.values.length > 1) bubble(currentIndex)
+
     }
 
-    swap(index , targetIndex , container){
+    extractMax() {
+        const returnVal = this.values.shift();
+        this.values.unshift(this.values[this.values.length - 1]);
+        this.values.pop();
+        const bubbleDown = (index) => {
+            let swapTargetIndex = -1;
+            const leftChildIndex =  this.getLeftChildIndex(index);
+            const rightChildIndex = this.getRightChildIndex(index);
+            const rightChildValue = this.values[rightChildIndex];
+            const leftChildValue = this.values[leftChildIndex];
+            const biggerVal = Math.max(rightChildValue,leftChildValue);
+            if(biggerVal === rightChildValue){
+                swapTargetIndex = rightChildIndex
+            }
+            if(biggerVal === leftChildValue){
+                swapTargetIndex = leftChildIndex
+            }
+           
+            if(this.values[index] < biggerVal){
+                this.swap(index , swapTargetIndex , this.values);
+                bubbleDown(swapTargetIndex);
+            }
+
+           
+           
+        }
+        bubbleDown(0);
+        return returnVal;
+    }
+
+    swap(index, targetIndex, container) {
         const temp = container[index];
-        container[index] =  container[targetIndex];
+        container[index] = container[targetIndex];
         container[targetIndex] = temp;
-        return container 
+        return container
     }
 
-    getParentIndex(childIndex){
-        return Math.floor((childIndex-1)/2);
+    getParentIndex(childIndex) {
+        return Math.floor((childIndex - 1) / 2);
     }
 
-    printAll(){
+    printAll() {
         console.log(this.values)
+    }
+
+    getLeftChildIndex(parentIndex) {
+        return 2*parentIndex +1;
+    }
+    getRightChildIndex(parentIndex){
+        return 2*parentIndex +2; 
     }
 }
 
@@ -48,4 +85,9 @@ maxBinaryHeap.insert(18);
 maxBinaryHeap.insert(27);
 maxBinaryHeap.insert(12);
 maxBinaryHeap.insert(55);
+
 maxBinaryHeap.printAll();
+maxBinaryHeap.insert(199);
+maxBinaryHeap.printAll();
+console.log('extractMax',maxBinaryHeap.extractMax());
+maxBinaryHeap.printAll('afterextract', maxBinaryHeap.printAll());
